e0a038022c8295ef91069ed792473b3d2e8c2ab3
sajal <mdsajalcse@gmail.com>
2023-10-16 23:18:35 +0600

    Explanation of arrays

---
+++ 1-D_Array_CSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿// CSharp 1 Dimensional Array
+
+
+string[] names = new string[]
+{
+    "Rasel","Rahi","Sajal","Rana","Rafsa","Rakhi","Rani"
+};
+/*names[0] = "sajal";
+names[1] = "Rana";
+names[2] = "Raju";
+names[3] = "Rajib";
+names[4] = "Saleh";
+names[5] = "Rajin";
+names[6] = "Ratul";*/
+
+
+
+int[] intArray = new int[]
+{
+    8, 9, 3, 4, 5, 6, 7
+};
+
+for (int i = 0; i < intArray.Length; i++)
+{
+    Console.WriteLine(names[i]);
+    Console.WriteLine("======");
+    Console.WriteLine(intArray[i]);
+
+   
+
+
+}
+
+Console.WriteLine("Arrays Dimensional::");
+Console.WriteLine(intArray.Rank);
+Console.WriteLine("Arrays Length::");
+Console.WriteLine(intArray.Length);
+Console.WriteLine("Arrays Number of Elements");
+
+
+
+
