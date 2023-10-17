09bd27fc7d12dfe4f8214ff0240c96efa990ffae
sajal <mdsajalcse@gmail.com>
2023-10-17 21:29:08 +0600

    two dimensional array

--- 2-Dimensional-Arrays/Program.cs
+++ 2-Dimensional-Arrays/Program.cs
@@ -6,17 +6,31 @@ string [,] dinnerItem = new string[,]
 
     {"Tomato","Fulkopi","Shim","Badhakopi"},
 
-    {"Onion","pasta","sweet","bread" }
+    {"Onion","pasta","sweet","bread" },
+
+    {"Onion","pasta","sweet","bread" },
+
+    {"Tomato","Fulkopi","Shim","Badhakopi"}
 
 };
+Console.WriteLine("Ratul::");
+Console.Write("=======");
+Console.WriteLine(dinnerItem[0, 0] + " " + dinnerItem[0, 1] + " " + dinnerItem[0,2]+ " " + dinnerItem[0,3]);
+Console.WriteLine("Raja::");
+Console.Write("=======");
+Console.WriteLine(dinnerItem[1, 0] + " " + dinnerItem[1, 1] + " " + dinnerItem[1, 2] + " " + dinnerItem[1, 3]);
+Console.WriteLine("Rani::");
+Console.Write("=======");
+Console.WriteLine(dinnerItem[2, 0] + " " + dinnerItem[2, 1] + " " + dinnerItem[2, 2] + " " + dinnerItem[2, 3]);
+Console.WriteLine("Rajib::");
+Console.Write("=======");
+Console.WriteLine(dinnerItem[3, 0] + " " + dinnerItem[3, 1] + " " + dinnerItem[3, 2] + " " + dinnerItem[3, 3]);
+Console.WriteLine("Rasel::");
+Console.Write("=======");
+Console.WriteLine(dinnerItem[4, 0] + " " + dinnerItem[4, 1] + " " + dinnerItem[4, 2] + " " + dinnerItem[4, 3]);
 
- 
-
-for(int i = 0; i < dinnerItem.Length; i++)
-{
 
 
-    Console.WriteLine(dinnerItem[0][1]);
-}
+
 
 
