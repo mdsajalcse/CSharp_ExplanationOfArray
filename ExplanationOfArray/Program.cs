80230635f425d1cfd467a1989bd3a962cbc2f7f0
sajal <mdsajalcse@gmail.com>
2023-10-17 22:13:49 +0600

    explanation of array

--- ExplanationOfArray/Program.cs
+++ ExplanationOfArray/Program.cs
@@ -1,2 +1,47 @@
 ﻿// C# Explanation of Array
 
+
+
+using System.Globalization;
+
+string[][] arrayItem = new string [4][];
+
+
+arrayItem[0] = new string[2];
+arrayItem[1] = new string[3];
+arrayItem[2]= new string[4];
+arrayItem[3]= new string[5];
+
+arrayItem[0][0] = "sajal";
+arrayItem[0][1] = "Rajib";
+
+int[] arr = new int[]
+{
+    2,1, 3, 4, 6, 5, 7, 8, 9, 10,
+};
+
+int[] arr1 = new int[]
+{
+    12,1, 3, 4, 76, 5, 17, 8, 9, 10,
+};
+
+Array.Copy(arr, arr1, arr.Length);
+Console.WriteLine(arr[0]);
+
+Array.Sort(arr);
+Console.WriteLine(arr);
+Console.WriteLine(arr[0]);
+Array.Reverse(arr);
+Console.WriteLine(arr);
+Console.WriteLine(arr[0]);
+
+
+
+Console.WriteLine(arrayItem[0][1]);
+
+Console.WriteLine(arrayItem.Rank);
+
+Console.WriteLine(arrayItem.GetLength(0));
+
+Console.WriteLine(arrayItem.Length);
+Console.WriteLine(arrayItem.GetType);
